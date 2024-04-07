6f6df6ea69ef3a1a082e9b1ff3adbd41bb46112b
Luis <luislacoste@rocketpin.com>
2024-04-07 20:33:30 -0300

    Update devServer configuration in vue.config.js

--- vue.config.js
+++ vue.config.js
@@ -3,6 +3,6 @@ const { defineConfig } = require('@vue/cli-service');
 module.exports = defineConfig({
   transpileDependencies: true,
   devServer: {
-    disableHostCheck: true
+    allowedHosts: 'all'
   }
 });
