584cca93381308f02d4f0f6e8e53d25bfb9f0667
Matías <137196771+Matias24120@users.noreply.github.com>
2024-04-08 18:40:43 -0300

    Update RequestsView.vue

--- src/views/RequestsView.vue
+++ src/views/RequestsView.vue
@@ -13,14 +13,14 @@
         <p :class="{ 'bg-blue-500': currentStep === 2, 'bg-gray-500': currentStep !== 2 }" class="border rounded-full px-2.5 py-0.5 text-white mr-2">2</p>
         <span>Añadir firmantes</span>
       </div>
-      <div class="border-b-2 border-gray-300 px-6 mt-1 mx-2"></div>
+      <!-- <div class="border-b-2 border-gray-300 px-6 mt-1 mx-2"></div>
       <div class="flex items-center">
         <p :class="{ 'bg-blue-500': currentStep === 3, 'bg-gray-500': currentStep !== 3 }" class="border rounded-full px-2.5 py-0.5 text-white mr-2">3</p>
         <span>Posicionar firmas</span>
-      </div>
+      </div> -->
       <div class="border-b-2 border-gray-300 px-6 mt-1 mx-2"></div>
       <div class="flex items-center">
-        <p :class="{ 'bg-blue-500': currentStep === 4, 'bg-gray-500': currentStep !== 4 }" class="border rounded-full px-2.5 py-0.5 text-white mr-2">4</p>
+        <p :class="{ 'bg-blue-500': currentStep === 3, 'bg-gray-500': currentStep !== 3 }" class="border rounded-full px-2.5 py-0.5 text-white mr-2">3</p>
         <span>Enviar documentos</span>
       </div>
     </div>
@@ -33,8 +33,8 @@
       <div class="m-6" @dragover.prevent @drop.prevent="handleDrop">
         <label for="fileInput" class="w-96 h-48 flex flex-col items-center text-center px-4 py-6 bg-blue-100 hover:bg-blue-200 rounded-lg shadow-lg cursor-pointer">
           <i class="fas fa-file-pdf fa-3x mt-2 mb-6"></i>
-          <span>Suelta el archivo <span class="font-bold">PDF o DOCX</span> aquí o <span class="text-blue-500 font-bold">haz click</span> para buscar en tus documentos.</span>
-          <input id="fileInput" type='file' accept=".pdf,.docx" class="hidden" @change="handleFileChange" multiple />
+          <span>Suelta el archivo <span class="font-bold">PDF, DOCX o JPG/PNG</span> aquí o <span class="text-blue-500 font-bold">haz click</span> para buscar en tus documentos.</span>
+          <input id="fileInput" type='file' accept=".pdf, .docx, .png, .jpg" class="hidden" @change="handleFileChange" multiple />
         </label>
       </div>
 
@@ -71,9 +71,33 @@
 
       <!-- Campos para los firmantes -->
       <div class="m-6">
-        <div v-for="(signer, index) in signers" :key="index" class="flex items-center">
-          <input type="text" v-model="signer.name" placeholder="Nombre del firmante" class="border-gray-600 border-b w-96 p-2 mr-4 mb-4">
-          <button @click="removeSigner(index)" v-show="signers.length > 1" class="bg-red-500 hover:bg-red-600 text-white px-2 py-1 rounded-md"><i class="fas fa-trash"></i></button>
+        <div v-for="(signer, index) in signers" :key="index" class="flex items-center mb-4">
+          <div class="flex flex-col">
+            <input type="text" v-model="signer.name" :placeholder="index === 0 ? 'Nombre del firmante' : 'Nombre del firmante ' + (index + 1)" class="border-gray-600 border-b w-108 p-2 mb-2">
+            <input type="text" v-model="signer.lastName" :placeholder="index === 0 ? 'Apellido del firmante' : 'Apellido del firmante ' + (index + 1)" class="border-gray-600 border-b w-108 p-2 mb-2">
+            <input type="number" v-model="signer.dni" :placeholder="index === 0 ? 'DNI del firmante' : 'DNI del firmante ' + (index + 1)" class="border-gray-600 border-b w-108 p-2 mb-2">
+            <div class="flex items-center mb-2">
+              <select v-model="signer.areaCode" class="border-gray-600 border-b p-2 mr-2 w-36">
+                <option value="" disabled selected>Área</option>
+                <option value="+591">+591 (BO)</option>
+                <option value="+549">+549 (ARG)</option>
+                <option value="+55">+55 (BR)</option>
+                <option value="+569">+569 (CL)</option>
+                <option value="+57">+57 (CO)</option>
+                <option value="+593">+593 (EC)</option>
+                <option value="+34">+34 (ES)</option>
+                <option value="+49">+49 (DE)</option>
+                <option value="+1">+1 (US)</option>
+                <option value="+521">+521 (MX)</option>
+                <option value="+507">+507 (PA)</option>
+                <option value="+51">+51 (PE)</option>
+                <option value="+598">+598 (UY)</option>
+                <option value="+58">+58 (VE)</option>
+              </select>
+              <input type="number" v-model="signer.phoneNumber" :placeholder="index === 0 ? 'Número de teléfono' : 'Número de teléfono ' + (index + 1)" class="border-gray-600 border-b p-2 w-72">
+            </div>
+          </div>
+          <button @click="removeSigner(index)" v-show="signers.length > 1" class="bg-red-500 hover:bg-red-600 text-white px-2 py-1 ml-4 mt-8 rounded-md"><i class="fas fa-trash"></i></button>
         </div>
         <button @click="addSigner" class="bg-blue-500 hover:bg-blue-600 text-gray-200 px-6 py-1 font-bold rounded-full">
           <i class="fas fa-plus mr-2"></i>Agregar
@@ -82,7 +106,7 @@
     </div>
 
     <!-- Paso 3: Posicionar las firmas -->
-    <div v-if="currentStep === 3">
+    <!-- <div v-if="currentStep === 3">
       <h2 class="text-2xl m-6">Posiciona las firmas <span class="text-gray-400">(opcional)</span></h2>
 
       <div class="m-6">
@@ -93,18 +117,15 @@
       </div>
 
       <div v-if="documentLoaded" class="m-6">
-        <!-- PDF -->
+        PDF 
         <div v-for="(documentUrl, index) in documentUrls" :key="index" class="mb-16">
           <iframe :src="documentUrl" class="w-full h-96" frameborder="0"></iframe>
         </div>
-        
-        <!-- DOCX -->
-        <!-- <embed :src="documentUrl" type="application/vnd.openxmlformats-officedocument.wordprocessingml.document" class="w-full h-96"></embed> -->
       </div>
-    </div>
+    </div> -->
 
-    <!-- Paso 4: Enviar el documento -->
-    <div v-if="currentStep === 4">
+    <!-- Paso 3: Enviar el documento -->
+    <div v-if="currentStep === 3">
       <h2 class="text-2xl m-6">Enviar documento</h2>
     </div>
 
@@ -137,11 +158,11 @@ export default {
       fileNames: [],
       errorMessage: '',
       documentSelected: false,
-      signers: [{ name: '' }],
+      signers: [{ name: '', lastName: '', dni: '', areaCode: '', phoneNumber: '' }],
       signerError: false,
-      signerErrorMessage: 'Por favor, escribe el nombre de todos los firmantes.',
-      documentLoaded: false,
-      documentUrls: []
+      signerErrorMessage: 'Por favor, completa todos los datos de los firmantes.',
+      // documentLoaded: false,
+      // documentUrls: []
     };
   },
   methods: {
@@ -160,10 +181,12 @@ export default {
         return; // No avanzar al siguiente paso
       }
       
-      // Verificar si hay algún nombre de firmante faltante solo si ya se han agregado firmantes
-      if (this.currentStep === 2 && this.signers.length > 0 && this.signers.some(signer => signer.name.trim() === '')) {
-        this.signerError = true;
-        return; // No avanzar al siguiente paso
+      // Verificar si todos los campos del firmante están completos
+      if (this.currentStep === 2 && this.signers.some(signer => {
+          return signer.name.trim() === '' || signer.lastName.trim() === '' || String(signer.dni).trim() === '' || signer.areaCode.trim() === '' || String(signer.phoneNumber).trim() === '';
+      })) {
+          this.signerError = true;
+          return; // No avanzar al siguiente paso
       }
 
       // Reiniciar el mensaje de error de los firmantes si retrocede después de haber agregado un documento
@@ -189,8 +212,8 @@ export default {
       for (let i = 0; i < files.length; i++) { // Simular la carga de cada archivo
         if (this.isValidFileType(files[i])) {
           // Almacenar el documento cargado
-          this.documentUrl = URL.createObjectURL(files[i]);
-          this.documentLoaded = true;
+          // this.documentUrl = URL.createObjectURL(files[i]);
+          // this.documentLoaded = true;
           setTimeout(() => {
             this.fileNames.push(files[i].name); // Agregar el nombre del archivo al array fileNames
             if (i === files.length - 1) { // Ocultar la barra de carga después de cargar todos los archivos
@@ -213,9 +236,9 @@ export default {
 
       for (let i = 0; i < files.length; i++) {
         if (this.isValidFileType(files[i])) {
-          const fileUrl = URL.createObjectURL(files[i]); // Almacenar la URL del documento cargado
-          this.documentUrls.push(fileUrl); // Agregar la URL del documento al array documentUrls
-          this.documentLoaded = true;
+          // const fileUrl = URL.createObjectURL(files[i]); // Almacenar la URL del documento cargado
+          // this.documentUrls.push(fileUrl); // Agregar la URL del documento al array documentUrls
+          // this.documentLoaded = true;
           setTimeout(() => {
             this.fileNames.push(files[i].name);
             if (i === files.length - 1) {
@@ -224,7 +247,7 @@ export default {
             }
           }, 1000 * (i + 1)); // Tiempo de simulación de carga
         } else {
-          this.errorMessage = 'Solo se permiten archivos PDF o DOCX.';
+          this.errorMessage = 'Solo se permiten archivos PDF, DOCX o JPG/PNG.';
           this.loading = false;
           break;
         }
@@ -240,20 +263,24 @@ export default {
       this.errorMessage = ''; 
     },
     isValidFileType(file) {
-      const allowedExtensions = ['.pdf', '.docx'];
+      const allowedExtensions = ['.pdf', '.docx', '.jpg', '.png'];
       const fileType = '.' + file.name.split('.').pop();
       return allowedExtensions.includes(fileType.toLowerCase());
     },
     addSigner() {
-      this.signers.push({ name: '' }); // Agregar un nuevo objeto de firmante con un campo de nombre vacío
+      this.signers.push({ name: '', lastName: '', dni: '', areaCode: '', phoneNumber: '' }); // Agregar un nuevo objeto de firmante con campos vacíos
     },
     removeSigner(index) {
       // Verificar si hay más de un firmante antes de eliminar
       if (this.signers.length > 1) {
         this.signers.splice(index, 1); // Eliminar el firmante en la posición especificada
       } else {
-        // Limpiar el nombre del primer firmante en lugar de eliminarlo
+        // Limpiar los campos del primer firmante en lugar de eliminarlo
         this.signers[index].name = '';
+        this.signers[index].lastName = '';
+        this.signers[index].dni = '';
+        this.signers[index].areaCode = '';
+        this.signers[index].phoneNumber = '';
       }
     }
   }
