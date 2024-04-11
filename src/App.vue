0a7d732b11ec13804bd5bfdbea86e078e4b888fe
Matías <137196771+Matias24120@users.noreply.github.com>
2024-04-11 09:09:48 -0300

    Update App.vue

--- src/App.vue
+++ src/App.vue
@@ -6,35 +6,20 @@
 
 <!-- TODO: Considerar en eliminar localSroage por seguridad y buena practica -->
 <script>
-import { setCookie, eraseCookie, getCookie} from './helpers/cookies';
-
-async function createUser(email) {
-  const response = await fetch(`https://firmasxw.com/api/createUsrOnLogin/${email}`, {
-    method: 'POST',
-    headers: {
-      'Content-Type': 'application/json',
-    },
-  });
-  // TODO: Considerar en manejar errores de forma mas adecuada
-  if (!response.ok) {
-    throw new Error('Error creating user');
-  }
-}
+import { setCookie, eraseCookie} from './helpers/cookies';
 
 export default {
   data() {
     return {
-      // user: JSON.parse(localStorage.getItem('user')) || null // Recuperar el usuario del almacenamiento local al cargar la aplicación
-      user: JSON.parse(getCookie('user')) || null // Recuperar el usuario del almacenamiento local al cargar la aplicación
+      user: JSON.parse(localStorage.getItem('user')) || null // Recuperar el usuario del almacenamiento local al cargar la aplicación
     };
   },
   methods: {
-    async handleLogin(userData) {
+    handleLogin(userData) {
       this.user = userData;
       setCookie('user', JSON.stringify(userData.email), 7); // Save mail as cookie
       localStorage.setItem('user', JSON.stringify(userData)); // Almacenar el usuario en el almacenamiento local
-      await createUser(userData.email)
-      this.$router.push('/requests');
+      this.$router.push('/requests'); 
     },
     handleLogout() {
       this.user = null;
