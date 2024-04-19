4c36191d42b9a1bee220c779bd4ae7efeed68e78
Matías <137196771+Matias24120@users.noreply.github.com>
2024-04-18 22:44:20 -0300

    Update RequestsView.vue

--- src/views/RequestsView.vue
+++ src/views/RequestsView.vue
@@ -31,116 +31,49 @@
           </thead>
           <tbody>
             <tr class="border-b" v-for="(solicitud, index) in todasLasSolicitudes" :key="'row-' + index">
-              <div class="pl-6 pr-12 py-2 items-center">
-                <td class="text-sm">{{ solicitud.track_id }}.pdf</td>
-                <i class="fas fa-chevron-down cursor-pointer text-blue-400"></i>
-              </div>
-              <td class="px-12 pb-6 text-sm"> 
-                <!-- {{ solicitud.firmantes[0].name }} {{ solicitud.firmantes[0].last_name }} -->
-                2/3
-              </td>
-              <td class="px-12 pb-6 text-sm">{{ solicitud.created_at }}</td>
-              <td class="px-12 pb-6 text-sm">
-                <div class="flex items-center">
-                  <span class="bg-green-400 text-white font-semibold px-3 py-0.5 rounded-xl mr-4 w-24 text-center"><!--{{ solicitud.status }}-->Firmado</span>
-                  <i class="far fa-file-pdf text-3xl text-blue-400"></i>
+              <td class="text-sm pl-6 pr-12 py-2 items-center">
+                {{ solicitud.track_id }}.pdf
+                <div v-show="isRowExpanded(index)" class="flex flex-col text-sm text-gray-700">
+                  <span><i class="fas fa-circle text-green-400 mr-2 mt-4"></i>{{ solicitud.firmantes[0].name }} {{ solicitud.firmantes[0].last_name }}</span>
+                  <div class="flex items-center mt-1 ml-6">
+                    <a href="https://sign.fxw.com/" target="_blank" class="underline hover:text-blue-400">https://sign.fxw.com/</a><i class="fas fa-clone ml-12 text-blue-400 hover:text-blue-300 cursor-pointer"></i>
+                  </div>
+                  <p class="mt-1 ml-6 mb-2">WhatsApp <span class="ml-4">+5491133847624</span></p>
+                </div>
+                <div v-show="isRowExpanded(index)" class="flex flex-col text-sm">
+                  <span><i class="fas fa-circle text-green-400 mr-2"></i>{{ solicitud.firmantes[1].name }} {{ solicitud.firmantes[1].last_name }}</span>
+                  <div class="flex items-center mt-1 ml-6">
+                    <a href="https://sign.fxw.com/" target="_blank" class="underline hover:text-blue-400">https://sign.fxw.com/</a><i class="fas fa-clone ml-12 text-blue-400 hover:text-blue-300 cursor-pointer"></i>
+                  </div>
+                  <p class="mt-1 ml-6 mb-2">WhatsApp <span class="ml-4">+5491133847624</span></p>
+                </div>
+                <div v-show="isRowExpanded(index)" class="flex flex-col text-sm">
+                  <span><i class="fas fa-circle text-gray-400 mr-2"></i>{{ solicitud.firmantes[0].name }} {{ solicitud.firmantes[0].last_name }}</span>
+                  <div class="flex items-center mt-1 ml-6">
+                    <a href="https://sign.fxw.com/" target="_blank" class="underline hover:text-blue-400">https://sign.fxw.com/</a><i class="fas fa-clone ml-12 text-blue-400 hover:text-blue-300 cursor-pointer"></i>
+                  </div>
+                  <p class="mt-1 ml-6 mb-2">WhatsApp <span class="ml-4">+5491133847624</span></p>
+                </div>
+                <div @click="toggleRow(index)" class="flex items-center cursor-pointer">
+                  <i class="fas fa-chevron-down text-blue-400 text-xl" :class="{'transform rotate-180': isRowExpanded(index)}"></i>
                 </div>
               </td>
-            </tr>
-            <tr v-for="(solicitud, index) in todasLasSolicitudes" :key="'row-' + index + '-2'">
-              <div class="pl-6 pr-12 py-2 items-center">
-                <td class="text-sm">{{ solicitud.track_id }}.pdf</td>
-                <i class="fas fa-chevron-down cursor-pointer text-blue-400"></i>
-              </div>
-              <td class="px-12 py-2 text-sm">
-                <!-- {{ solicitud.firmantes[0].name }} {{ solicitud.firmantes[0].last_name }} -->
-                3/4
+              <td class="px-12 pt-2 text-sm" style="vertical-align: top;"> 
+                2/3
               </td>
-              <td class="px-12 py-2 text-sm">{{ solicitud.created_at }}</td>
-              <td class="px-12 py-2 text-sm">
+              <td class="px-12 pt-2 text-sm" style="vertical-align: top;">
+                {{ solicitud.created_at }}
+              </td>
+              <td class="px-12 text-sm" style="vertical-align: top;">
                 <div class="flex items-center">
-                  <span class="bg-red-400 text-white font-semibold px-3 py-0.5 rounded-xl mr-4 w-24 text-center"><!--{{ solicitud.status }}-->Rechazado</span>
-                  <i class="far fa-file-pdf text-3xl text-gray-400"></i>
+                  <span class="bg-green-400 text-white font-semibold px-3 py-0.5 rounded-xl mr-4 w-24 text-center"><!--{{ solicitud.status }}-->Firmado</span>
+                  <i class="far fa-file-pdf text-3xl text-blue-400 pt-1.5"></i>
                 </div>
               </td>
             </tr>
           </tbody>
         </table>
       </div>
-
-      <!-- <h3 class="text-center mt-4 font-bold">Links de documentos para firmar</h3>
-
-    <div v-if="documentosFirmados.length > 0">
-      <ul class="text-center mt-4">
-        <li v-for="documento in documentosFirmados" :key="documento.track_id">
-          <div>Track ID: {{ documento.track_id }}</div>
-          <div>Fecha de Creación: {{ documento.created_at }}</div>
-          <div v-if="documento.urls && documento.urls.length > 0">
-            <h4>Links de Firma:</h4>
-            <ul>
-              <li v-for="url in documento.urls" :key="url.link">
-                <div>Link: <a :href="url.link" target="_blank">{{ url.link }}</a></div>
-                <div>Persona: {{ url.person }}</div>
-                <div>DNI: {{ url.dni }}</div>
-                <div>Teléfono: {{ url.phone }}</div>
-              </li>
-            </ul>
-          </div>
-        </li>
-      </ul>
-    </div>
-
-    <p v-else class="text-center mt-4">No hay documentos firmados.</p>
-
-    <h3 class="text-center mt-4 font-bold">Estado de Documento</h3>
-
-    <div class="text-center" v-if="documentoStatus">
-      <div>Estado: {{ documentoStatus.status }}</div>
-      <div v-if="documentoStatus.pdf_url">URL del PDF: {{ documentoStatus.pdf_url }}</div>
-      <div v-if="documentoStatus.signed_at">Firmado en: {{ documentoStatus.signed_at }}</div>
-    </div>
-
-    <h3 class="text-center mt-4 font-bold">Solicitudes de un grupo</h3>
-
-    <div v-if="solicitudesGrupo.length > 0">
-      <ul class="text-center mt-4">
-        <li v-for="solicitud in solicitudesGrupo" :key="solicitud.track_id" class="border-b border-red-500">
-          <div>Track ID: {{ solicitud.track_id }}</div>
-          <div>Original document: {{solicitud.original_document}}</div>
-          <div>Signed document: {{solicitud.signed_document}}</div>
-          <div>Estado: {{ solicitud.status }}</div>
-          <div>Cantidad de Firmantes: {{ solicitud.cantidad_firmantes }}</div>
-          <div>Cantidad Firmados: {{ solicitud.cantidad_firmados }}</div>
-          <div>Firmantes: {{ solicitud.firmantes }}</div>
-          <div>Firma en todas las hojas: {{ solicitud.firma_en_todas_las_hojas }}</div>
-          <div>Creado: {{ solicitud.created_at }}</div>
-          <div>Webhook: {{ solicitud.webhook }}</div>
-          <div>Cliente: {{ solicitud.cliente }}</div>
-        </li>
-      </ul>
-    </div>
-    <p v-else class="text-center mt-4">No hay solicitudes para este grupo.</p>
-
-    <h3 class="text-center mt-4 font-bold">Lista de todas las solicitudes</h3>
-
-    <div v-if="todasLasSolicitudes.length > 0">
-      <ul class="text-center mt-4">
-        <li v-for="solicitud in todasLasSolicitudes" :key="solicitud.track_id" class="border-b border-red-500">
-          <div>Track ID: {{ solicitud.track_id }}</div>
-          <div>Original document: {{solicitud.original_document}}</div>
-          <div>Signed document: {{solicitud.signed_document}}</div>
-          <div>Estado: {{ solicitud.status }}</div>
-          <div>Cantidad de Firmantes: {{ solicitud.cantidad_firmantes }}</div>
-          <div>Cantidad Firmados: {{ solicitud.cantidad_firmados }}</div>
-          <div>Firmantes: {{ solicitud.firmantes }}</div>
-          <div>Firma en todas las hojas: {{ solicitud.firma_en_todas_las_hojas }}</div>
-          <div>Creado: {{ solicitud.created_at }}</div>
-          <div>Webhook: {{ solicitud.webhook }}</div>
-          <div>Cliente: {{ solicitud.cliente }}</div>
-        </li>
-      </ul>
-    </div>
-    <p v-else class="text-center mt-4">No hay solicitudes.</p> -->
     </main>
 </template>
 
@@ -160,7 +93,8 @@ export default {
       documentosFirmados: [],
       documentoStatus: null,
       solicitudesGrupo: [],
-      todasLasSolicitudes: []
+      todasLasSolicitudes: [],
+      expandedRows: []
     };
   },
   created() {
@@ -175,6 +109,20 @@ export default {
     },
     toggleSidebar() {
       this.sidebarOpen = !this.sidebarOpen;
+    },
+    // Método para alternar la expansión de filas
+    toggleRow(index) {
+      if (this.expandedRows.includes(index)) {
+        // Si ya está expandido, lo cerramos
+        this.expandedRows = this.expandedRows.filter(i => i !== index);
+      } else {
+        // Si no está expandido, lo abrimos
+        this.expandedRows.push(index);
+      }
+    },
+    // Método para verificar si una fila está expandida
+    isRowExpanded(index) {
+      return this.expandedRows.includes(index);
     },
     fetchDocumentosFirmados() {
       // Realiza la solicitud POST a la API para obtener los documentos firmados
