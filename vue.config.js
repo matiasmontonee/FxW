04533772e707fed90ac4ee76ddbe55d808e984e0
Luis Lacoste <luis.lacoste@hotmail.com>
2024-05-14 15:12:24 -0300

    fix vue config

--- vue.config.js
+++ vue.config.js
@@ -1,9 +1,20 @@
-const { defineConfig } = require('@vue/cli-service');
+// vue.config.js
+const { defineConfig } = require('@vue/cli-service')
 
 module.exports = defineConfig({
-  transpileDependencies: true,
-  devServer: {
-    allowedHosts: 'all'
-  },
-  publicPath: '/web/',
-});
+  // options...
+})
+
+
+
+
+
+// const { defineConfig } = require('@vue/cli-service');
+
+// module.exports = defineConfig({
+//   transpileDependencies: true,
+//   devServer: {
+//     allowedHosts: 'all'
+//   },
+//   publicPath: '/',
+// });
