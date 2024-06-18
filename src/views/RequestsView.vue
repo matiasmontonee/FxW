8f708a6f20fae1ca9ef9c4503b4749ae7d68efd8
Luis Lacoste <luis.lacoste@hotmail.com>
2024-06-18 13:59:48 -0300

    fix calendar position

--- src/views/RequestsView.vue
+++ src/views/RequestsView.vue
@@ -15,18 +15,17 @@
               <div class="flex items-center">
                 <i class="fas fa-search mr-2 text-blue-400 cursor-pointer"></i>
                 <input type="text" v-model="searchTerm" @input="searchDocuments"
-                  class="text-gray-400 focus:outline-none" placeholder="Id Documento" />
+                  class="text-gray-400 text-lg focus:outline-none" placeholder="Id Documento" />
               </div>
             </th>
             <th class="w-1/5 pr-10 pt-5 pb-3 text-left text-sm">
-              <p class="text-gray-400">Firmantes</p>
+              <p class="text-gray-400 text-lg">Firmantes</p>
             </th>
             <!-- Buscador fecha -->
             <th class="w-1/5 pt-5 pb-3 text-left text-sm">
               <div class="flex items-center">
-                <i class="fas fa-search mr-2 text-blue-400 cursor-pointer"></i>
                 <input type="date" v-model="searchDate" @input="searchDocuments"
-                  class="text-gray-400 focus:outline-none" placeholder="Fecha de Creación" />
+                  class="text-gray-400 text-lg focus:outline-none" placeholder="Fecha de Creación" />
               </div>
             </th>
             <!-- Buscador estado -->
@@ -34,7 +33,7 @@
               <div class="flex items-center">
                 <i class="fas fa-chevron-down mr-2 text-blue-400 cursor-pointer" @click="toggleDropdown"></i>
                 <div class="relative">
-                  <button class="text-gray-400 focus:outline-none" @click="toggleDropdown">Estado</button>
+                  <button class="text-gray-400 text-lg focus:outline-none" @click="toggleDropdown">Estado</button>
                   <ul v-if="showDropdown" class="absolute top-full bg-white border border-gray-300 shadow-md ">
                     <li class="cursor-pointer px-4 py-2 hover:bg-gray-100" @click="setSearchStatus('Firmado')">Firmado
                     </li>
@@ -49,7 +48,7 @@
               </div>
             </th>
             <th class="w-1/5 pr-10 pt-5 pb-3 text-left text-sm">
-              <p class="text-gray-400">Informe</p>
+              <p class="text-gray-400 text-lg focus:outline-none">Informe</p>
             </th>
           </tr>
         </thead>
@@ -80,7 +79,7 @@
                         <i v-else class="fas fa-check-circle ml-2 mr-1 text-green-400"></i>
                       </div>
                       <p class="mt-1 ml-6 mb-2">{{ solicitud.firmantes[0]?.method }} <span class="ml-4">{{
-                          isNaN(solicitud.firmantes[0]?.phone.charAt(0)) ? solicitud.firmantes[0]?.phone : '+' +
+                        isNaN(solicitud.firmantes[0]?.phone.charAt(0)) ? solicitud.firmantes[0]?.phone : '+' +
                           solicitud.firmantes[0]?.phone ?? solicitud.firmantes[0]?.email }}</span></p>
                     </div>
                     <!-- TWO -->
@@ -99,7 +98,7 @@
                         <i v-else class="fas fa-check-circle ml-2 mr-1 text-green-400"></i>
                       </div>
                       <p class="mt-1 ml-6 mb-2">{{ solicitud.firmantes[1]?.method }} <span class="ml-4">{{
-                          isNaN(solicitud.firmantes[1]?.phone.charAt(0)) ? solicitud.firmantes[1]?.phone : '+' +
+                        isNaN(solicitud.firmantes[1]?.phone.charAt(0)) ? solicitud.firmantes[1]?.phone : '+' +
                           solicitud.firmantes[1]?.phone ?? solicitud.firmantes[1]?.email }}</span></p>
                     </div>
                     <!-- THREE -->
@@ -118,7 +117,7 @@
                         <i v-else class="fas fa-check-circle ml-2 mr-1 text-green-400"></i>
                       </div>
                       <p class="mt-1 ml-6 mb-2">{{ solicitud.firmantes[2]?.method }} <span class="ml-4">{{
-                          isNaN(solicitud.firmantes[2]?.phone.charAt(0)) ? solicitud.firmantes[2]?.phone : '+' +
+                        isNaN(solicitud.firmantes[2]?.phone.charAt(0)) ? solicitud.firmantes[2]?.phone : '+' +
                           solicitud.firmantes[2]?.phone ?? solicitud.firmantes[2]?.email }}</span></p>
                     </div>
                   </div>
@@ -140,7 +139,7 @@
                         <i v-else class="fas fa-check-circle ml-2 mr-1 text-green-400"></i>
                       </div>
                       <p class="mt-1 ml-6 mb-2">{{ solicitud.firmantes[3]?.method }} <span class="ml-4">{{
-                          isNaN(solicitud.firmantes[3]?.phone.charAt(0)) ? solicitud.firmantes[3]?.phone : '+' +
+                        isNaN(solicitud.firmantes[3]?.phone.charAt(0)) ? solicitud.firmantes[3]?.phone : '+' +
                           solicitud.firmantes[3]?.phone ?? solicitud.firmantes[3]?.email }}</span></p>
                     </div>
                     <!-- FIVE -->
@@ -159,7 +158,7 @@
                         <i v-else class="fas fa-check-circle ml-2 mr-1 text-green-400"></i>
                       </div>
                       <p class="mt-1 ml-6 mb-2">{{ solicitud.firmantes[4]?.method }} <span class="ml-4">{{
-                          isNaN(solicitud.firmantes[4]?.phone.charAt(0)) ? solicitud.firmantes[4]?.phone : '+' +
+                        isNaN(solicitud.firmantes[4]?.phone.charAt(0)) ? solicitud.firmantes[4]?.phone : '+' +
                           solicitud.firmantes[4]?.phone ?? solicitud.firmantes[4]?.email }}</span></p>
                     </div>
                     <!-- SIX -->
@@ -178,14 +177,14 @@
                         <i v-else class="fas fa-check-circle ml-2 mr-1 text-green-400"></i>
                       </div>
                       <p class="mt-1 ml-6 mb-2">{{ solicitud.firmantes[5]?.method }} <span class="ml-4">{{
-                          isNaN(solicitud.firmantes[5]?.phone.charAt(0)) ? solicitud.firmantes[5]?.phone : '+' +
+                        isNaN(solicitud.firmantes[5]?.phone.charAt(0)) ? solicitud.firmantes[5]?.phone : '+' +
                           solicitud.firmantes[5]?.phone ?? solicitud.firmantes[5]?.email }}</span></p>
                     </div>
                   </div>
                 </div>
               </div>
             </td>
-            <td class="pl-5 pt-2 text-sm" style="vertical-align: top;">
+            <td class="pl-9 pt-2 text-sm" style="vertical-align: top;">
               <p>{{ solicitud.cantidad_firmados }}/{{ solicitud.cantidad_firmantes }}</p>
             </td>
             <td class="pl-8 pt-2 text-sm" style="vertical-align: top;">
@@ -197,7 +196,7 @@
                 {{ solicitud.is_finished ? 'Firmado' : 'Pendiente' }}
               </p>
             </td>
-            <td class="pl-4 text-sm" style="vertical-align: top;">
+            <td class="pl-6 text-sm" style="vertical-align: top;">
               <a :href="solicitud.firmantes[0]?.link" target="_blank">
                 <i class="far fa-file-pdf text-3xl text-blue-400 hover:text-blue-300"></i>
               </a>
@@ -317,7 +316,11 @@ export default {
       const day = date.getDate().toString().padStart(2, '0');
       const month = (date.getMonth() + 1).toString().padStart(2, '0'); // Months are zero-based
       const year = date.getFullYear();
-      return `${year}-${month}-${day}`;
+      return `${day}-${month}-${year}`;
+    },
+    formatDateToDDMMYYYY(dateString) {
+      const [year, month, day] = dateString.split('-');
+      return `${day}-${month}-${year}`;
     },
     setSearchStatus(status) {
       this.searchStatus = status === 'Firmado';
@@ -331,7 +334,7 @@ export default {
       const searchTerm = this.searchTerm.trim().toLowerCase();
       const searchDate = this.searchDate;
       const searchStatus = this.searchStatus;
-
+      console.log(searchDate);
       const todasLasSolicitudesOriginal = [...this.todasLasSolicitudesOriginal];
 
       // Si tanto el término de búsqueda como la fecha de búsqueda están vacíos, mostrar todas las solicitudes
@@ -340,12 +343,12 @@ export default {
         return;
       }
 
-      
+
 
       // Filtrar las solicitudes por ID de documento y fecha de creación
       this.todasLasSolicitudes = todasLasSolicitudesOriginal.filter(solicitud => {
         const matchesSearchTerm = !searchTerm || solicitud.id_custom_client.toString().toLowerCase().includes(searchTerm);
-        const matchesSearchDate = !searchDate || this.formatDate(solicitud.created_at) === searchDate;
+        const matchesSearchDate = !searchDate || this.formatDate(solicitud.created_at) === this.formatDateToDDMMYYYY(searchDate);
         const matchesSearchStatus = searchStatus === '' || solicitud.is_finished === searchStatus;
         return matchesSearchTerm && matchesSearchDate && matchesSearchStatus;
       });
@@ -359,7 +362,7 @@ export default {
 
       axios.get(`https://firmasxw.com/test/list?page=${this.currentPage}`, { headers })
         .then(response => {
-          this.todasLasSolicitudes = response.data.splice(0, 10);
+          this.todasLasSolicitudes = response.data.splice(0, 15);
           this.todasLasSolicitudesOriginal = this.todasLasSolicitudes;
           this.loading = false;
         })
@@ -374,7 +377,7 @@ export default {
       this.fetchTodasLasSolicitudes();
     },
     nextPage() {
-      if (this.todasLasSolicitudes.length === 10) {
+      if (this.todasLasSolicitudes.length === 15) {
         this.currentPage += 1;
         console.log(this.currentPage);
         this.fetchTodasLasSolicitudes();
@@ -392,3 +395,11 @@ export default {
   }
 }
 </script>
+
+
+<!-- El icono de fecha -->
+<style>
+::-webkit-calendar-picker-indicator {
+  margin-left: -15px;
+}
+</style>
